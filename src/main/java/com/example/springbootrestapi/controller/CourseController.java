13f72f2fcb8660658436cf8aac5a70b2d302f910
WilDevp <wilmar.garciava@gmail.com>
2023-01-14 14:02:02 -0500

    docs(global): Add Documentatios with Swagger
    Se agrega documentacion de los servicios rest con swagger

--- src/main/java/com/example/springbootrestapi/controller/CourseController.java
+++ src/main/java/com/example/springbootrestapi/controller/CourseController.java
@@ -3,44 +3,60 @@ package com.example.springbootrestapi.controller;
 import com.example.springbootrestapi.model.Course;
 import com.example.springbootrestapi.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
     private CourseService courseService;
+
     @Autowired
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
     }
+
     @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
     public Iterable<Course> getAllCourses() {
         return courseService.getCourses();
     }
+
     @GetMapping("{id}")
+    @ResponseStatus(code = HttpStatus.OK)
     public Optional<Course> getCourseById(@PathVariable("id") long courseId) {
         return courseService.getCourseById(courseId);
     }
+
     @GetMapping("category/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
     public Iterable<Course> getCourseByCategory(@PathVariable("name") String category) {
         return courseService.getCoursesByCategory(category);
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Course createCourse(@RequestBody Course course) {
         return courseService.createCourse(course);
     }
+
     @PutMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void updateCourse(@PathVariable("id") long courseId, @RequestBody Course course) {
         courseService.updateCourse(courseId, course);
     }
+
     @DeleteMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void deleteCourseById(@PathVariable("id") long courseId) {
         courseService.deleteCourseById(courseId);
     }
+
     @DeleteMapping
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void deleteCourses() {
         courseService.deleteCourses();
     }
